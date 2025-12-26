ff539979ba0ac2a699e7f585c2c397128ad77cd7
Tatiana Podoliak <tatiana.podoliak@bs2.lt>
2025-12-26 16:28:24 +0200

    task3: implement cli interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("info.picocli:picocli:4.7.7")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true, // <-- включает -h и -V
+        version = "gendiff 1.0"
+)
+public class App implements Callable<Integer> {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
