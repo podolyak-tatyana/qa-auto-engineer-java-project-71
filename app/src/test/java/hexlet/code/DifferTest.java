a9956c374b824308d371c6edc62ee8d2019de1fe
Tatiana Podoliak <tatiana.podoliak@bs2.lt>
2025-12-28 17:51:52 +0200

    fixes for checkstyle

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
 
-    private static final String filepath1 = "src/test/resources/file.json";
-    private static final String filepath2 = "src/test/resources/file2.json";
+    private static final String FILE_PATH_1 = "src/test/resources/file.json";
+    private static final String FILE_PATH_2 = "src/test/resources/file2.json";
 
     @Test
     void generate() throws IOException {
@@ -23,7 +23,7 @@ class DifferTest {
                   + verbose: true
                 }""";
 
-        var result = Differ.generate(filepath1, filepath2, "");
+        var result = Differ.generate(FILE_PATH_1, FILE_PATH_2, "");
         assertEquals(expected, result);
     }
 }
