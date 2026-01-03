f1a99699590ace3466657856595eceef4d168910
Tatiana Podoliak <tatiana.podoliak@bs2.lt>
2026-01-03 11:26:01 +0200

    task 9 : update tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -14,34 +14,41 @@ class DifferTest {
     private static final String YML_FILE_PATH_1 = "src/test/resources/dataset/yml/file1.yml";
     private static final String YML_FILE_PATH_2 = "src/test/resources/dataset/yml/file2.yml";
 
-    @Test
-    void generate() throws IOException {
-        var expected = """
+    private static final String expected = """
                 {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
 
+    @Test
+    void generate() throws IOException {
         var result = Differ.generate(FILE_PATH_1, FILE_PATH_2, "json");
         assertEquals(expected, result);
     }
 
     @Test
     void generateForYml() throws IOException {
-        var expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-
         var result = Differ.generate(YML_FILE_PATH_1, YML_FILE_PATH_2, "yml");
         assertEquals(expected, result);
     }
